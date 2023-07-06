cc76283ff9d3be53e74e8288d94366c4a61ff3a8
Arjun Raj <arjunraj.pala@gmail.com>
2023-07-06 08:52:01 +0200

    Adding read aloud feature for each story

--- pages/Stories-Hub.py
+++ pages/Stories-Hub.py
@@ -2,6 +2,9 @@ import streamlit as st
 import requests
 import json
 import random
+import os
+import re
+from gtts import gTTS
 
 # Define the API endpoint
 API_ENDPOINT = "https://mongo-gcp-project.uc.r.appspot.com/api/v1/"
@@ -21,6 +24,10 @@ def get_story():
     #st.write(story)
     return story
 
+# Cleaning up the text from emojis
+def remove_emoji(text):
+    return re.sub(r'[^\x00-\x7F]+', '', text)
+
 def main():
     data= get_story()
     story_data = json.loads(data)
@@ -31,6 +38,15 @@ def main():
         st.subheader(f'Genre: {story["genre"]}')
         with st.expander("📚🚀 Ready for an Adventure? Unfold a Magical Tale Here! 🧙‍♀️🌟 "):
             st.write(story["story"])
+            if st.button("Read Aloud!",type="primary",key=f'{story["id"]}',help="Generates an audio file for the story."):
+                with st.spinner("Generating your story into an audio file...."):
+                    cleaned_input = remove_emoji(story["story"])
+                    clean_title=remove_emoji(f'{story["title"]}')
+                    audio_file_name=f"{clean_title}"
+                    st.info("Read the story aloud by playing the below audio file!")    
+                    speech = gTTS(text = cleaned_input, lang='en-uk', slow = False)
+                    speech.save(f"{audio_file_name}.mp3")
+                    st.audio(f"{audio_file_name}.mp3", format='audio/mp3')  
 
 if __name__ == "__main__":
     main()
